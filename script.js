3b0c9d1d33d6e5c713ba64c51082d2a106c80193
lovnight48 <vicentebecerrafigueroa@gmail.com>
2026-01-23 02:42:32 -0300

    Confirmar

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const texto = "Tengo algo especial preparado para ti...";
 let i = 0;
 
 const fotos = [
-  "img/foto1.jpg",
-    "img/foto2.jpg",
-      "img/foto3.jpg"
+  "IMG_20251111_201622_652.webp",
+    "IMG_20251113_222809_373.webp",
+      "IMG_20251106_164405_188.webp"
       ];
 
       let indiceFoto = 0;
